abde7ab5827a63a787d0646ab22ed188316c5e5c
Astha Kumari <astha.kri50@gmail.com>
2021-02-04 17:55:28 +0530

    Bit Manipulation II

--- BitManipulation/SingleNumberIII.java
+++ BitManipulation/SingleNumberIII.java
@@ -1,16 +1,24 @@
 public class Solution {
-    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
-    public int singleNumber(final int[] A) {
-        int ans = 0;
-        for(int i = 0 ; i < 32 ; i++){
-            int count = 0 ; 
-            for(int j = 0 ; j < A.length ; j++){
-                if(  ((1<<i)&A[j]) > 0)
-                count++ ; 
-            }
-            if(count % 3 != 0)
-            ans |= (1<<i) ;
+    public int[] solve(int[] A) {
+
+        int aXorb = 0;  // the result of a xor b;
+        for (int item : A) aXorb ^= item;
+        
+        int lastBit = (aXorb & (aXorb - 1)) ^ aXorb;  // the last bit that a diffs b
+        
+        int intA = 0, intB = 0;
+        for (int item : A) {
+        
+            // based on the last bit, group the items into groupA(include a) and groupB
+            if ( (item & lastBit) != 0) intA = intA ^ item;
+            else intB = intB ^ item;
         }
-        return ans ;
+       
+        int x = Math.min(intA, intB), y =  Math.max(intA, intB);
+        int[] ans = new int[2];
+        ans[0] = x;
+        ans[1] = y;
+        return ans; 
+        
     }
 }
