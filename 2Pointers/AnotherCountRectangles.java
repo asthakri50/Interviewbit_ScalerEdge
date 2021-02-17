ee8f977f748f784e53151d423580cba025873d7c
Astha Kumari <astha.kri50@gmail.com>
2021-02-17 19:49:33 +0530

    Easy

---
+++ 2Pointers/AnotherCountRectangles.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int solve(int[] A, int B) {
+        int n = A.length ;
+        int p1 = 0 ; 
+        int p2 =  n-1;
+        long count = 0;
+        while(p1 <= p2){
+            if( ( (long)A[p1] * A[p2]) < B){
+                count += 2*(p2-p1+1) -1 ;
+                p1++;
+            }
+            else
+            p2--;
+        }
+        count = count % ( (int)Math.pow(10,9) + 7);
+        return (int)count ;
+    }
+}
