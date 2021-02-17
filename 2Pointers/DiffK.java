85801788f6d295dff1b9a0e9ec41d7987ee25510
Astha Kumari <astha.kri50@gmail.com>
2021-02-18 01:46:47 +0530

    Medium

--- 2Pointers/DiffK.java
+++ 2Pointers/DiffK.java
@@ -10,6 +10,9 @@ public class Solution {
             else if(A[r] - A[l] > B)
                 l++;
             else{
+                //When B == 0 , we need to find A[r] -A[r] such that i !=j i.e check for duplicates in array
+                // But, in first iteration only, A[r] - A[l] >0 (in general) then l++ , now l == r => A[l] - A[r] == 0 , we need to do r++
+                // Check for [1, 2, 3]
                 if( l== r)
                     r++;
                 else    
