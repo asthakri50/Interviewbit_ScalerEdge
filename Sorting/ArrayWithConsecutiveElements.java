5ceaec956de401e0b280eecd2b8d3fbfa7a016d2
Astha Kumari <astha.kri50@gmail.com>
2021-03-14 20:24:38 +0530

    Easy

--- Sorting/ArrayWithConsecutiveElements.java
+++ Sorting/ArrayWithConsecutiveElements.java
@@ -1,11 +1,12 @@
+import java.util.Arrays;
 public class ArrayWithConsecutiveElements {
     public int solve(int[] A) {
-        Arrays.sort(A) ;
-        for(int i = 0 ; i < A.length-1 ; i++){
-            if(A[i+1] - A[i] != 1)
-            return 0;
+        Arrays.sort(A);
+        for (int i = 0; i < A.length - 1; i++) {
+            if (A[i + 1] - A[i] != 1)
+                return 0;
         }
         return 1;
     }
-    
+
 }
--- Stack/BalancedParenthesis.java
+++ Stack/BalancedParenthesis.java
@@ -2,37 +2,35 @@ public class Solution {
     public int solve(String A) {
         Stack<Character> open_stack = new Stack();
         Stack<Character> close_stack = new Stack();
-        for(int i = 0 ; i < A.length() ; i++){
-            if(A.charAt(i) == '[' )
-                open_stack.push(A.charAt(i) );
-            else if(A.charAt(i) == '{' )
-                open_stack.push(A.charAt(i) );  
-            else if(A.charAt(i) == '(' )
-                open_stack.push(A.charAt(i) );   
-                
-            if(A.charAt(i) ==')' ){
-                if(!open_stack.isEmpty() && open_stack.peek() == '(' )
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) == '[')
+                open_stack.push(A.charAt(i));
+            else if (A.charAt(i) == '{')
+                open_stack.push(A.charAt(i));
+            else if (A.charAt(i) == '(')
+                open_stack.push(A.charAt(i));
+
+            if (A.charAt(i) == ')') {
+                if (!open_stack.isEmpty() && open_stack.peek() == '(')
                     open_stack.pop();
                 else
-                    close_stack.push(A.charAt(i) );
-            } 
-            else if(A.charAt(i) =='}' ){
-                if(!open_stack.isEmpty() && open_stack.peek() == '{' )
+                    close_stack.push(A.charAt(i));
+            } else if (A.charAt(i) == '}') {
+                if (!open_stack.isEmpty() && open_stack.peek() == '{')
                     open_stack.pop();
                 else
-                    close_stack.push(A.charAt(i) );
-            } 
-            else if(A.charAt(i) ==']' ){
-                if(!open_stack.isEmpty() && open_stack.peek() == '[' )
+                    close_stack.push(A.charAt(i));
+            } else if (A.charAt(i) == ']') {
+                if (!open_stack.isEmpty() && open_stack.peek() == '[')
                     open_stack.pop();
                 else
-                    close_stack.push(A.charAt(i) );
-            } 
+                    close_stack.push(A.charAt(i));
+            }
         }
-    if(open_stack.isEmpty() && close_stack.isEmpty() )
-        return 1;
-        
-    return 0;
-    
+        if (open_stack.isEmpty() && close_stack.isEmpty())
+            return 1;
+
+        return 0;
+
     }
 }
