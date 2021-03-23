6964032d007313c5d1cdadce71db4b96ddacfcd3
Astha Kumari <astha.kri50@gmail.com>
2021-03-23 23:12:32 +0530

    Easy

--- Stack/BalancedParenthesis.java
+++ Stack/BalancedParenthesis.java
@@ -1,4 +1,58 @@
-public class Solution {
+// Problem Description
+
+// Given an expression string A, examine whether the pairs and the orders of “{“,”}”, ”(“,”)”, ”[“,”]” are correct in A.
+
+// Refer to the examples for more clarity.
+
+
+
+// Problem Constraints
+// 1 <= |A| <= 100
+
+
+
+// Input Format
+// The first and the only argument of input contains the string A having the paranthesis sequence.
+
+
+
+// Output Format
+// Return 0, if the paranthesis sequence is not balanced.
+
+// Return 1, if the paranthesis sequence is balanced.
+
+
+
+// Example Input
+// Input 1:
+
+//  A = {([])}
+// Input 2:
+
+//  A = (){
+// Input 3:
+
+//  A = ()[] 
+
+
+// Example Output
+// Output 1:
+
+//  1 
+// Output 2:
+
+//  0 
+// Output 3:
+
+//  1 
+
+
+// Example Explanation
+// You can clearly see that the first and third case contain valid paranthesis.
+
+// In the second case, there is no closing bracket for {, thus the paranthesis sequence is invalid.
+
+    public class Solution {
     public int solve(String A) {
         Stack<Character> open_stack = new Stack();
         Stack<Character> close_stack = new Stack();
