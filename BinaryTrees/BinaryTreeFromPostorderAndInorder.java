b1d0b518f06fe19b564a01f3c2f399fb71e1de53
Astha Kumari <astha.kri50@gmail.com>
2021-04-26 23:55:02 +0530

    Medium

--- BinaryTrees/BinaryTreeFromPostorderAndInorder.java
+++ BinaryTrees/BinaryTreeFromPostorderAndInorder.java
@@ -1,7 +1,7 @@
-// Binary Tree From Inorder And Preorder
+// Binary Tree From Inorder And Postorder
 // Problem Description
 
-// Given preorder and inorder traversal of a tree, construct the binary tree.
+// Given inorder and postorder traversal of a tree, construct the binary tree.
 
 // NOTE: You may assume that duplicates do not exist in the tree.
 
@@ -9,9 +9,9 @@
 // 1 <= number of nodes <= 105
 
 // Input Format
-// First argument is an integer array A denoting the preorder traversal of the tree.
+// First argument is an integer array A denoting the inorder traversal of the tree.
 
-// Second argument is an integer array B denoting the inorder traversal of the tree.
+// Second argument is an integer array B denoting the postorder traversal of the tree.
 
 // Output Format
 // Return the root node of the binary tree.
@@ -19,12 +19,12 @@
 // Example Input
 // Input 1:
 
-//  A = [1, 2, 3]
-//  B = [2, 1, 3]
+//  A = [2, 1, 3]
+//  B = [2, 3, 1]
 // Input 2:
 
-//  A = [1, 6, 2, 3]
-//  B = [6, 1, 3, 2]
+//  A = [6, 1, 3, 2]
+//  B = [6, 3, 2, 1]
 
 // Example Output
 // Output 1:
@@ -52,32 +52,27 @@
 public class Solution {
 
     public TreeNode buildTree(ArrayList<Integer> A, ArrayList<Integer> B) {
-        if (A.size() == 0)
-            return null;
-
-        int pre_strt = 0;
-        int pre_end = A.size() - 1;
         int in_strt = 0;
-        int in_end = B.size() - 1;
-
-        return builder(A, B, pre_strt, pre_end, in_strt, in_end);
+        int in_end = A.size() - 1;
+        int po_strt = 0;
+        int po_end = B.size() - 1;
+        return builder(A, B, in_strt, in_end, po_strt, po_end);
     }
 
-    public TreeNode builder(ArrayList<Integer> A, ArrayList<Integer> B, int pre_strt, int pre_end, int in_strt,
-            int in_end) {
-        // Base Case
+    public TreeNode builder(ArrayList<Integer> A, ArrayList<Integer> B, int in_strt, int in_end, int po_strt,
+            int po_end) {
         if (in_strt > in_end)
             return null;
 
-        TreeNode root = new TreeNode(A.get(pre_strt));
+        TreeNode root = new TreeNode(B.get(po_end));
 
         if (in_strt == in_end)
             return root;
 
-        int i = findIdx(B, in_strt, in_end, root.val);
+        int i = findIdx(A, in_strt, in_end, root.val);
 
-        root.left = builder(A, B, pre_strt + 1, pre_strt + i - in_strt, in_strt, i - 1);
-        root.right = builder(A, B, pre_strt + i - in_strt + 1, pre_end, i + 1, in_end);
+        root.left = builder(A, B, in_strt, i - 1, po_strt, po_strt + i - in_strt - 1);
+        root.right = builder(A, B, i + 1, in_end, po_strt + i - in_strt, po_end - 1);
 
         return root;
     }
