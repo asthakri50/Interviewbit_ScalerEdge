83e1e39c8d7dc115bd5d667b5b09e2a70b52c9df
Astha Kumari <astha.kri50@gmail.com>
2021-05-03 00:10:12 +0530

    Easy

---
+++ Arrays/PattenPrinting1.java
@@ -0,0 +1,67 @@
+// Pattern Printing -1
+// Problem Description
+
+// Print a Pattern According to The Given Value of A.
+
+// Example: For A = 3 pattern looks like:
+
+// 1
+
+// 1 2
+
+// 1 2 3
+
+
+
+// Problem Constraints
+// 1 <= A <= 1000
+
+
+// Input Format
+// First and only argument is an integer denoting A.
+
+
+
+// Output Format
+// Return a two-dimensional array where each row in the returned array represents a row in the pattern.
+
+
+
+// Example Input
+// Input 1:
+
+//  A = 3
+// Input 2:
+
+//  A = 4
+
+
+// Example Output
+// Output :1
+
+//  [ 
+//    [1]
+//    [1, 2]
+//    [1, 2, 3]
+//  ]
+// Output 2:
+
+//  [ 
+//    [1]
+//    [1, 2]
+//    [1, 2, 3]
+//    [1, 2, 3, 4]
+//  ]
+
+
+// Example Explanation
+// Explanation 2:
+
+ 
+//  For A = 4 pattern looks like:  
+//                              1
+//                              1 2
+//                              1 2 3
+//                              1 2 3 4
+//  So we will return it as two-dimensional array.
+
